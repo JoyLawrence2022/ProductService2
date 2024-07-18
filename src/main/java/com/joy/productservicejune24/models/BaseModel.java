668cc496cd0557b30ec59a86a9b7539faca3b2af
Joy Lawrence <joyforeever@gmail.com>
2024-07-18 20:02:20 +0530

    added DB connection

--- src/main/java/com/joy/productservicejune24/models/BaseModel.java
+++ src/main/java/com/joy/productservicejune24/models/BaseModel.java
@@ -1,5 +1,9 @@
 package com.joy.productservicejune24.models;
 
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.MappedSuperclass;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -7,7 +11,10 @@ import java.util.Date;
 
 @Getter
 @Setter
+@MappedSuperclass
 public class BaseModel{
+    @Id  // sets id as the Primary Key
+    @GeneratedValue(strategy = GenerationType.IDENTITY)  // sets PK to auto-increment
     private long id;
     private Date createdAt;
     private Date updatedAt;
--- src/main/java/com/joy/productservicejune24/models/Product.java
+++ src/main/java/com/joy/productservicejune24/models/Product.java
@@ -1,13 +1,17 @@
 package com.joy.productservicejune24.models;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.ManyToOne;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
+@Entity
 public class Product extends BaseModel{
 //    private long id;
     private String title;
     private double price;
+    @ManyToOne
     private Category category;
 }
